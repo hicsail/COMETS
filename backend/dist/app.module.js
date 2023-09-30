871059e9eda7c98305cfe872887dc4dd0b04d8c7
Rishi Shah <shahrishi108@gmail.com>
2023-09-30 15:33:15 -0400

    connected job to app module

--- backend/dist/app.module.js
+++ backend/dist/app.module.js
@@ -11,12 +11,13 @@ const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const mongoose_1 = require("@nestjs/mongoose");
+const job_module_1 = require("./job/job.module");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [mongoose_1.MongooseModule.forRoot('mongodb://127.0.0.1:27017/comets')],
+        imports: [mongoose_1.MongooseModule.forRoot('mongodb://127.0.0.1:27017/comets'), job_module_1.JobModule],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JobModule } from './job/job.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/comets')],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/comets'), JobModule],
   controllers: [AppController],
   providers: [AppService],
 })
