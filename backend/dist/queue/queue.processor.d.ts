ae889c93e90760a98184c6f570648d080343c181
Hazim Ab Halim <hazim.halim54@yahoo.com>
2023-11-06 10:13:32 -0500

    removed dist and node_modules

--- backend/dist/queue/queue.processor.d.ts
+++
@@ -1,7 +0,0 @@
-import { Job as BullJob } from 'bull';
-export declare class QueueProcessor {
-    private readonly logger;
-    handleAddJob(job: BullJob<{
-        id: string;
-    }>): Promise<void>;
-}
