17bcf3dd5a3e6ad88668a804a1afc685b9b34693
Rishi Shah <shahrishi108@gmail.com>
2024-01-04 09:02:05 -0800

    cleanup

--- backend/dist/dispatcher/dispatcher.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { DispatcherService } from './dispatcher.service';
-export declare class DispatcherController {
-    private dispatcherService;
-    constructor(dispatcherService: DispatcherService);
-    dispatch(): Promise<void>;
-}
--- backend/dist/dispatcher/dispatcher.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Queue } from 'bull';
-export declare class DispatcherService {
-    private queue;
-    private serverStatus;
-    constructor(queue: Queue);
-    initializeQueueListener(): void;
-    dispatch(): Promise<void>;
-}
