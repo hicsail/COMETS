3203c13a4e45d8e8753515ce939e724ca3594a3f
Rishi Shah <shahrishi108@gmail.com>
2024-01-16 17:13:14 -0500

    added prop validators

--- backend/src/job/schemas/job.schema.ts
+++ backend/src/job/schemas/job.schema.ts
@@ -6,39 +6,38 @@ export type JobDocument = HydratedDocument<Job>;
 @Schema()
 export class Job {
   
-  @Prop()
+  @Prop({ required: true, trim: true })
   name: string;
 
-  @Prop()
+  @Prop({ required: true, type: Number, min: [0, 'Value must be positive'] })
   defaultVmax: number;
 
-  @Prop()
+  @Prop({ required: true, type: Number, min: [0, 'Value must be positive'] })
   defaultKm: number;
 
-  @Prop()
+  @Prop({ required: true, type: Number, min: [1, 'Value must be at least 1'] })
   maxCycles: number;
 
-  @Prop()
+  @Prop({ required: true, type: Number, min: [0, 'Value must be positive'] })
   timeStep: number;
 
-  @Prop()
+  @Prop({ required: true, type: Number, min: [0, 'Value must be positive'] })
   spaceWidth: number;
 
-  @Prop()
+  @Prop({ required: true, type: Number, min: [0, 'Value must be positive'] })
   maxSpaceBiomass: number;
 
-  @Prop()
+  @Prop({ required: true, type: Number, min: [0, 'Value must be non-negative'] })
   minSpaceBiomass: number;
 
-  @Prop()
+  @Prop({ required: true, type: Boolean })
   writeMediaLog: boolean;
 
-  @Prop()
+  @Prop({ required: true, type: Boolean })
   writeFluxLog: boolean;
 
-  @Prop()
+  @Prop({ required: true, type: Number, min: [0, 'Value must be non-negative'] })
   fluxLogRate: number;
-
 }
 
 export const JobSchema = SchemaFactory.createForClass(Job);
