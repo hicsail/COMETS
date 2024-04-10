a93532fae4bd71bec4426ee02d02708b2e7f8847
Rishi Shah <shahrishi108@gmail.com>
2024-04-10 09:44:13 -0400

    centered footer in drawer

--- frontend/src/pages/SummaryReview.tsx
+++ frontend/src/pages/SummaryReview.tsx
@@ -1,210 +1,157 @@
-import React, {
-  useState
-} from 'react';
-import {
-  Box,
-  Button,
-  Card,
-  Drawer,
-  Grid,
-  TextField,
-  Typography
-} from "@mui/material";
-import {
-  ThemeProvider,
-  createTheme
-} from "@mui/material";
+import React, { useState } from 'react';
+import { Box, Button, Card, Drawer, Grid, TextField, Typography, ThemeProvider, createTheme } from "@mui/material";
 import FooterStepper from '../components/FooterStepper';
-
 
 const bodyTheme = createTheme({
   typography: {
-     h1: {
-        fontSize: 50,
-        fontWeight: 700
-     },
-     h2: {
-        fontSize: 40,
-     },
-     h3: {
-        fontSize: 18.
-
-     }
+    h1: {
+      fontSize: 50,
+      fontWeight: 700
+    },
+    h2: {
+      fontSize: 40,
+    },
+    h3: {
+      fontSize: 18
+    }
   }
 });
 
-
 export function SummaryReviewPage() {
   const [activeStep, setActiveStep] = useState(2);
-  return ( 
-     <
-     ThemeProvider theme = {
-        bodyTheme
-     } >
-     <
-     Box component = "main"
-     sx = {
-        {
-           position: "relative",
-           // width: "82vw",
-           height: "100vh",
 
-        }
-     } >
-     <
-     Grid container spacing = {
-        2
-     } >
-     <
-     Grid item xs = {
-        6
-     } >
-     <
-     Grid container spacing = {
-        2
-     }
-     direction = "column"
-     alignItems = "left"
-     style = {
-        {
-           paddingLeft: '2vw',
-           paddingBottom: "10%"
-        }
-     } >
-     <
-     Typography variant = "h1"
-     sx = {
-        {
-           color: "black",
-           textAlign: "left",
-           paddingBottom: "10%",
-           paddingTop: "5%"
-        }
-     } > 3. Review < /Typography>
+  return (
+    <ThemeProvider theme={bodyTheme}>
+      <Box 
+        component="main"
+        sx={{
+          position: "relative",
+          height: "100vh",
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Grid 
+              container 
+              spacing={2}
+              direction="column"
+              alignItems="left"
+              style={{
+                paddingLeft: '2vw',
+                paddingBottom: "10%"
+              }}
+            >
+              <Typography 
+                variant="h1"
+                sx={{
+                  color: "black",
+                  textAlign: "left",
+                  paddingBottom: "10%",
+                  paddingTop: "5%"
+                }}
+              >
+                3. Review
+              </Typography>
 
-     <
-     Typography variant = "h3"
-     sx = {
-        {
-           textAlign: "left",
-           color: "grey",
-           paddingBottom: "10%"
-        }
-     } >
-     Please review your selected simulation.Once you have confirmed the selection is correct, you can run your simulation by entering your email below. <
-     /Typography>
+              <Typography 
+                variant="h3"
+                sx={{
+                  textAlign: "left",
+                  color: "grey",
+                  paddingBottom: "10%"
+                }}
+              >
+                Please review your selected simulation. Once you have confirmed the selection is correct, you can run your simulation by entering your email below.
+              </Typography>
 
-     <
-     Typography variant = "h3"
-     sx = {
-        {
-           textAlign: "left",
-           color: "grey",
-           paddingBottom: "10%"
-        }
-     } >
-     We will notify you of your simulations results via email. <
-     /Typography>
+              <Typography 
+                variant="h3"
+                sx={{
+                  textAlign: "left",
+                  color: "grey",
+                  paddingBottom: "10%"
+                }}
+              >
+                We will notify you of your simulations results via email.
+              </Typography>
 
-     <
-     Card sx = {
-        {
-           width: '100%',
-           p: 2,
-           boxSizing: 'border-box',
-           borderRadius: '16px'
-        }
-     } >
-     <
-     Typography variant = "h6"
-     sx = {
-        {
-           mb: 2,
-           textAlign: "left"
-        }
-     } >
-     Continue with email <
-     /Typography>
+              <Card 
+                sx={{
+                  width: '100%',
+                  p: 2,
+                  boxSizing: 'border-box',
+                  borderRadius: '16px'
+                }}
+              >
+                <Typography 
+                  variant="h6"
+                  sx={{
+                    mb: 2,
+                    textAlign: "left"
+                  }}
+                >
+                  Continue with email
+                </Typography>
 
-     <
-     TextField fullWidth label = "Email address"
-     variant = "outlined"
-     sx = {
-        {
-           mb: 2
-        }
-     }
-     />
+                <TextField 
+                  fullWidth 
+                  label="Email address"
+                  variant="outlined"
+                  sx={{
+                    mb: 2
+                  }}
+                />
 
-     <
-     Typography variant = "body2"
-     sx = {
-        {
-           mb: 2
-        }
-     } >
-     By continuing, you agree to the confirmation of the selected simulation to be processed. <
-     /Typography>
+                <Typography 
+                  variant="body2"
+                  sx={{
+                    mb: 2
+                  }}
+                >
+                  By continuing, you agree to the confirmation of the selected simulation to be processed.
+                </Typography>
 
-     <
-     Button variant = "contained"
-     fullWidth >
-     Continue <
-     /Button> <
-     /Card> <
-     /Grid>
+                <Button 
+                  variant="contained"
+                  fullWidth
+                >
+                  Continue
+                </Button>
+              </Card>
+            </Grid>
+          </Grid>
 
+          <Grid item xs={6}>
+            <Typography 
+              sx={{
+                color: "black",
+                fontSize: "26px"
+              }}
+            >
+              Insert cards here
+            </Typography>
+          </Grid>
+        </Grid>
 
-     <
-     /Grid>
-
-     <
-     Grid item xs = {
-        6
-     } >
-     <
-     Typography sx = {
-        {
-           color: "black",
-           fontSize: "26px"
-        }
-     } > Insert cards here < /Typography> <
-     /Grid> <
-     /Grid>
-
-
-     
-
-     <
-     Drawer 
-     variant = "permanent"
-     anchor = "bottom"
-     PaperProps = {
-        {
-           sx: {
+        <Drawer
+          variant="permanent"
+          anchor="bottom"
+          PaperProps={{
+            sx: {
+              display: 'flex',        // Enable flexbox
+              justifyContent: 'center', // Center items horizontally
+              alignItems: 'center',   // Center items vertically
               background: "white",
               height: 100,
               width: "90vw",
-              left:'15vw',
+              left: '15vw',
               zIndex: 99,
-              alignItems: "center",
-
-           },
-        }
-     } >
-     <
-     FooterStepper activeStep = {
-        activeStep
-     }
-     /> 
-     
-     </Drawer>
-
-     </Box>
-
-
-     <
-     /ThemeProvider> 
-
+            },
+          }}
+        >
+          <FooterStepper activeStep={activeStep} />
+        </Drawer>
+      </Box>
+    </ThemeProvider>
   );
 }
