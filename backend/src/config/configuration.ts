c7dc7c10ede23b54188f10939b0d48c208e6cf61
Hazim Ab Halim <hazim.halim54@yahoo.com>
2024-07-01 12:59:25 -0400

    added validation

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -11,7 +11,10 @@ export default () => ({
   email: {
     username: process.env.EMAIL_USERNAME,
     password: process.env.EMAIL_PASSWORD
-  }
+  },
+  // redis: {
+  //   host: process.env.REDIS_HOST
+  // }
 
 });
 
--- backend/src/job/dto/create-job.dto.ts
+++ backend/src/job/dto/create-job.dto.ts
@@ -1,3 +1,5 @@
 export class CreateJobDto {
     readonly filepath?: string;
+    readonly model_info: any[];
+    readonly metabolites: any[];
 }
